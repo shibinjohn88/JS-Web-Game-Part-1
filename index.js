658f8f2e4c4a3734aee6a12af376a8d774997533
shibinjohn88 <shibinjohnpdlm@gmail.com>
2022-09-25 19:01:33 -0400

    java script added to create images

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+
+
+//function to add new images to the screen
+function newImage(src_path, left_position, bottom_position) {
+    let element = document.createElement('img');
+    element.src = src_path;
+    element.style.position = 'fixed';
+    element.style.left = left_position;
+    element.style.bottom = bottom_position;
+    document.body.append(element);
+    
+}
+
+//function to add and remove items.
+function newItem(src_path, left_position, bottom_position) {
+    let element = document.createElement('img');
+    element.src = src_path;
+    element.style.position = 'fixed';
+    element.style.left = left_position;
+    element.style.bottom = bottom_position;
+    document.body.append(element);
+    
+    //event listener to remove items when double clicked
+    element.addEventListener('dblclick', function(){
+        element.remove();
+    })
+}
+
+
+
+newImage('assets/tree.png', '200px', '300px');
+newImage('assets/pillar.png', '350px', '100px');
+newImage('assets/crate.png', '150px', '200px');
+newImage('assets/well.png', '500px', '425px');
+newImage('assets/green-character.gif', '100px', '100px');
+newImage('assets/pine-tree.png', '450px', '200px');
+newItem('assets/sword.png', '500px', '405px');
+newItem('assets/sheild.png', '165px', '185px');
+newItem('assets/staff.png', '600px', '100px');
+
+
+
+
